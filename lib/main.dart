d94fd9314fe05f6b8523544b69797a8f57f141ea
MahmoudElSayed96 <gcfjxvkj@gmail.com>
2024-07-07 20:00:47 +0300

    add font assets, add flutter screen utils and modify matrial app.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:akadomen/router/app_router.dart';
 import 'package:akadomen/utils/constants/routes.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,11 +12,26 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Akadomen',
-      debugShowCheckedModeBanner: false,
-      initialRoute: RouteManager.initialRoute,
-      onGenerateRoute: AppRouter.onGenerateRoute,
-    );
+    return ScreenUtilInit(
+        designSize: const Size(360, 690),
+        minTextAdapt: true,
+        splitScreenMode: true,
+        builder: (_, child) => MaterialApp(
+              builder: (context, widget) {
+                final mediaQueryData = MediaQuery.of(context);
+                final scaledMediaQueryData = mediaQueryData.copyWith(
+                  textScaler: TextScaler.noScaling,
+                );
+                return MediaQuery(
+                  data: scaledMediaQueryData,
+                  child: widget!,
+                );
+              },
+              theme: ThemeData(fontFamily: 'Poppins'),
+              title: 'akadomen',
+              debugShowCheckedModeBanner: false,
+              initialRoute: RouteManager.initialRoute,
+              onGenerateRoute: AppRouter.onGenerateRoute,
+            ));
   }
 }
