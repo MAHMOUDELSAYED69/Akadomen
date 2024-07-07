20572dca54aba95c73a2bd142cdd1cde4babbb3b
MahmoudElSayed96 <gcfjxvkj@gmail.com>
2024-07-07 13:15:50 +0300

    create folder stucture and start with splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Akadomen',
+      
       debugShowCheckedModeBanner: false,
     );
   }
