021d7e8c5eeafd20dfdb5774bb4b33643750dbd8
MahmoudElSayed96 <gcfjxvkj@gmail.com>
2024-07-12 18:36:22 +0300

    modify CalcCubit.

--- lib/controllers/calc/calccubit_cubit.dart
+++ lib/controllers/calc/calccubit_cubit.dart
@@ -1,20 +1,26 @@
 import 'package:bloc/bloc.dart';
+import 'package:akadomen/models/juice.dart';
 
-class CounterCubit extends Cubit<Map<String, int>> {
-  CounterCubit() : super({});
+class CalcCubit extends Cubit<Map<JuiceModel, int>> {
+  CalcCubit() : super({});
 
-  void increment(String juiceType) {
-    final currentCount = state[juiceType] ?? 0;
-    emit({...state, juiceType: currentCount + 1});
+  void increment(JuiceModel juice) {
+    final currentCount = state[juice] ?? 0;
+    emit({...state, juice: currentCount + 1});
   }
 
-  void decrement(String juiceType) {
-    final currentCount = state[juiceType] ?? 0;
+  void decrement(JuiceModel juice) {
+    final currentCount = state[juice] ?? 0;
     if (currentCount > 0) {
-      emit({...state, juiceType: currentCount - 1});
+      emit({...state, juice: currentCount - 1});
     }
   }
 
   void reset() => emit({});
-}
 
+  double getTotalPrice() {
+    return state.entries.fold<double>(0, (sum, entry) {
+      return sum + entry.key.price * entry.value;
+    });
+  }
+}
