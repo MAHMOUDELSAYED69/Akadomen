15524ea3875cf853322951a92f048b8f7422dd4c
MahmoudElSayed96 <gcfjxvkj@gmail.com>
2024-07-14 01:59:58 +0300

    add more cubits in approuter.

--- lib/views/screens/home.dart
+++ lib/views/screens/home.dart
@@ -1,4 +1,5 @@
-import 'package:akadomen/repositories/fruits.dart';
+import 'dart:io';
+
 import 'package:akadomen/utils/constants/colors.dart';
 import 'package:akadomen/utils/constants/routes.dart';
 import 'package:akadomen/utils/extentions/extentions.dart';
@@ -6,6 +7,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '../../controllers/calc/calccubit_cubit.dart';
+import '../../controllers/repo/fruits_repository.dart';
+import '../../models/juice.dart';
 import '../../utils/constants/images.dart';
 import '../widgets/add_remove.dart';
 import '../widgets/invoice_widget.dart';
@@ -17,95 +20,105 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final calculator = context.read<CalculatorCubit>();
-
     return Scaffold(
-      body: Container(
-        alignment: Alignment.center,
-        width: context.width,
-        height: context.height,
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            fit: BoxFit.cover,
-            image: AssetImage(ImageManager.homeBackground),
-          ),
-        ),
-        child: Stack(
-          children: [
-            Row(
+      body: BlocBuilder<FruitsRepositoryCubit, List<JuiceModel>>(
+        builder: (context, userJuiceList) {
+          return Container(
+            alignment: Alignment.center,
+            width: context.width,
+            height: context.height,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: AssetImage(ImageManager.homeBackground),
+              ),
+            ),
+            child: Stack(
               children: [
-                Expanded(
-                  child: GridView.builder(
-                    padding: EdgeInsets.only(
-                        left: context.width / 25,
-                        right: context.width / 25,
-                        top: 10,
-                        bottom: 50),
-                    itemCount: FruitsRepository.juiceList.length,
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                            crossAxisSpacing: 20,
-                            mainAxisSpacing: 20,
-                            crossAxisCount: 4),
-                    itemBuilder: (context, index) {
-                      final juice = FruitsRepository.juiceList[index];
-                      return GestureDetector(
-                        onTap: () => calculator.increment(juice),
-                        child: Stack(
-                          alignment: Alignment.bottomCenter,
-                          children: [
-                            Container(
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(30),
-                                boxShadow: [
-                                  BoxShadow(
-                                    offset: const Offset(1, -1),
-                                    color: ColorManager.brown.withOpacity(0.5),
-                                    blurRadius: 5,
-                                    spreadRadius: 2,
-                                  ),
-                                ],
-                                image: DecorationImage(
-                                  fit: BoxFit.cover,
-                                  image: AssetImage(juice.image!),
-                                ),
-                              ),
-                            ),
-                            ItemCard(
-                              price: juice.price,
-                              name: juice.name,
-                            ),
-                            AddAndRemoveCard(
-                              onDecrement: () => calculator.decrement(juice),
-                              onIncrement: () => calculator.increment(juice),
-                            )
-                          ],
+                Row(
+                  children: [
+                    Expanded(
+                      child: GridView.builder(
+                        padding: EdgeInsets.only(
+                          left: context.width / 25,
+                          right: context.width / 25,
+                          top: 10,
+                          bottom: 50,
                         ),
-                      );
-                    },
+                        itemCount: userJuiceList.length,
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisSpacing: 20,
+                          mainAxisSpacing: 20,
+                          crossAxisCount: 4,
+                        ),
+                        itemBuilder: (context, index) {
+                          final juice = userJuiceList[index];
+                          return GestureDetector(
+                            onTap: () => calculator.increment(juice),
+                            child: Stack(
+                              alignment: Alignment.bottomCenter,
+                              children: [
+                                Container(
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(30),
+                                    boxShadow: [
+                                      BoxShadow(
+                                        offset: const Offset(1, -1),
+                                        color:
+                                            ColorManager.brown.withOpacity(0.5),
+                                        blurRadius: 5,
+                                        spreadRadius: 2,
+                                      ),
+                                    ],
+                                    image: DecorationImage(
+                                      fit: BoxFit.cover,
+                                      image: FileImage(File(juice.image!)),
+                                    ),
+                                  ),
+                                ),
+                                ItemCard(
+                                  price: juice.price,
+                                  name: juice.name,
+                                ),
+                                AddAndRemoveCard(
+                                  onDecrement: () =>
+                                      calculator.decrement(juice),
+                                  onIncrement: () =>
+                                      calculator.increment(juice),
+                                ),
+                              ],
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                    const InvoiceWidget(),
+                  ],
+                ),
+                Positioned(
+                  left: 5,
+                  top: 5,
+                  child: IconButton(
+                    hoverColor: ColorManager.white,
+                    style: ButtonStyle(
+                      backgroundColor: WidgetStatePropertyAll(
+                        ColorManager.white.withOpacity(0.7),
+                      ),
+                    ),
+                    onPressed: () =>
+                        Navigator.pushNamed(context, RouteManager.settings),
+                    icon: Icon(
+                      size: 7.sp,
+                      Icons.settings,
+                      color: ColorManager.brown,
+                    ),
                   ),
                 ),
-                const InvoiceWidget(),
               ],
             ),
-            Positioned(
-              left: 5,
-              top: 5,
-              child: IconButton(
-                  hoverColor: ColorManager.white,
-                  style: ButtonStyle(
-                      backgroundColor: WidgetStatePropertyAll(
-                          ColorManager.white.withOpacity(0.7),),),
-                  onPressed: () => Navigator.pushNamed(
-                        context,RouteManager.settings
-                      ),
-                  icon: Icon(
-                    size: 7.sp,
-                    Icons.settings,
-                    color: ColorManager.brown,
-                  )),
-            ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
--- lib/views/screens/settings.dart
+++ lib/views/screens/settings.dart
@@ -1,18 +1,41 @@
-import 'package:akadomen/controllers/login/login_cubit.dart';
-import 'package:akadomen/controllers/logout/logout_cubit.dart';
+import 'dart:io';
+
 import 'package:akadomen/utils/constants/routes.dart';
 import 'package:akadomen/utils/extentions/extentions.dart';
-import 'package:akadomen/utils/helpers/my_snackbar.dart';
-import 'package:akadomen/views/widgets/custom_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
+import '../../controllers/logout/logout_cubit.dart';
+import '../../controllers/image/image_cubit.dart';
+import '../../controllers/repo/fruits_repository.dart';
+import '../../models/juice.dart';
 import '../../utils/constants/colors.dart';
 import '../../utils/constants/images.dart';
+import '../../utils/helpers/my_snackbar.dart';
+import '../../utils/helpers/shared_pref.dart';
+import '../../views/widgets/custom_button.dart';
 
-class SettingsScreen extends StatelessWidget {
+import '../widgets/custom_text_field.dart';
+
+class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
+
+  @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  final String _userName = CacheData.getData(key: 'currentUser');
+  File? _pickedImage;
+  late TextEditingController _nameController;
+  late TextEditingController _priceController;
+
+  @override
+  void initState() {
+    _nameController = TextEditingController();
+    _priceController = TextEditingController();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,92 +54,286 @@ class SettingsScreen extends StatelessWidget {
               ),
             ),
           ),
-          Row(
-            children: [
-              SizedBox(width: context.width / 8.7),
-              MyElevatedButton(
-                backgroundColor: ColorManager.white.withOpacity(0.7),
-                size: Size(context.width / 2, context.height / 2),
-                widget: Icon(
-                  Icons.add_a_photo_rounded,
-                  size: 20.sp,
-                  color: ColorManager.brown,
+          Padding(
+            padding: EdgeInsets.only(right: context.width / 2.7),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    BlocConsumer<PickImageCubit, PickImageState>(
+                      listener: (context, state) {
+                        if (state is PickImageSuccess) {
+                          _pickedImage = state.imagePath;
+                        }
+                        if (state is PickImageFailure) {
+                          customSnackBar(context, state.errMessage);
+                        }
+                      },
+                      builder: (context, state) {
+                        return ElevatedButton(
+                          style: ButtonStyle(
+                            padding:
+                                const WidgetStatePropertyAll(EdgeInsets.zero),
+                            overlayColor: const WidgetStatePropertyAll(
+                                ColorManager.white),
+                            backgroundColor: WidgetStatePropertyAll(
+                                ColorManager.white.withOpacity(0.7)),
+                            fixedSize: WidgetStatePropertyAll(
+                              Size(context.width / 3, context.height / 2),
+                            ),
+                          ),
+                          child: _pickedImage != null
+                              ? ClipRRect(
+                                  borderRadius: BorderRadius.circular(25),
+                                  child: Image.file(
+                                    width: double.infinity,
+                                    height: double.infinity,
+                                    _pickedImage!,
+                                    fit: BoxFit.cover,
+                                  ),
+                                )
+                              : Icon(Icons.add_a_photo_rounded,
+                                  size: 20.sp, color: ColorManager.brown),
+                          onPressed: () =>
+                              context.read<PickImageCubit>().pickImage(),
+                        );
+                      },
+                    ),
+                  ],
                 ),
-                onPressed: () {},
-              ),
-            ],
+                _buildJuiceForm(context)
+              ],
+            ),
           ),
           Positioned(
             left: 5,
             top: 5,
-            child: IconButton(
-                hoverColor: ColorManager.white,
-                style: ButtonStyle(
-                  backgroundColor: WidgetStatePropertyAll(
-                    ColorManager.white.withOpacity(0.7),
+            child: Column(
+              children: [
+                IconButton(
+                  hoverColor: ColorManager.white,
+                  style: ButtonStyle(
+                    backgroundColor: WidgetStatePropertyAll(
+                      ColorManager.white.withOpacity(0.7),
+                    ),
+                  ),
+                  onPressed: () => Navigator.pop(context),
+                  icon: Icon(
+                    size: 7.sp,
+                    Icons.arrow_back,
+                    color: ColorManager.brown,
                   ),
                 ),
-                onPressed: () => Navigator.pop(context),
-                icon: Icon(
-                  size: 7.sp,
-                  Icons.arrow_back,
-                  color: ColorManager.brown,
-                )),
+                SizedBox(height: 10.h),
+                Tooltip(
+                  margin: const EdgeInsets.only(top: 5),
+                  height: 34,
+                  message: 'Logout',
+                  decoration: BoxDecoration(
+                      color: ColorManager.brown,
+                      borderRadius: BorderRadius.circular(4)),
+                  child: BlocListener<AuthStatus, AuthStatusState>(
+                    listener: (context, state) {
+                      if (state is Logout) {
+                        Navigator.pushNamedAndRemoveUntil(
+                            context, RouteManager.login, (route) => false);
+                        customSnackBar(context, 'Logout Successfully!');
+                      }
+
+                      if (state is LogoutFailure) {
+                        customSnackBar(context, 'There was an error!');
+                      }
+                    },
+                    child: IconButton(
+                      hoverColor: ColorManager.white,
+                      style: ButtonStyle(
+                        backgroundColor: WidgetStatePropertyAll(
+                          ColorManager.white.withOpacity(0.7),
+                        ),
+                      ),
+                      icon: Icon(
+                        size: 7.sp,
+                        Icons.logout,
+                        color: ColorManager.brown,
+                      ),
+                      onPressed: () {
+                        context.read<AuthStatus>().logout();
+                      },
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
           Positioned(
             right: 0,
             top: 0,
             bottom: 0,
-            child: Row(
-              children: [
-                Container(
-                    margin: EdgeInsets.only(right: context.width / 30),
-                    padding: const EdgeInsets.all(20),
-                    width: context.width / 4.5,
-                    height: context.height / 1.2,
-                    decoration: BoxDecoration(
-                      color: ColorManager.white,
-                      borderRadius: BorderRadius.circular(10),
-                      boxShadow: [
-                        BoxShadow(
-                          offset: const Offset(1, -1),
-                          color: ColorManager.brown.withOpacity(0.5),
-                          blurRadius: 5,
-                          spreadRadius: 2,
-                        ),
-                      ],
-                    ),
-                    child: Column(
-                      children: [
-                        const Spacer(),
-                        BlocListener<LoginCubit, LoginState>(
-                          listener: (context, state) {
-                            if (state is Logout) {
-                              Navigator.pushNamedAndRemoveUntil(
-                                context,
-                                RouteManager.login,
-                                (route) => false,
-                              );
-                              if (state is LogoutFailure) {
-                                customSnackBar(context,
-                                    'There was an error please try again later!');
-                              }
-                            }
-                          },
-                          child: MyElevatedButton(
-                            title: 'Logout',
-                            onPressed: () {
-                              context.read<AuthStatus>().logout();
-                            },
-                          ),
-                        )
-                      ],
-                    ))
-              ],
+            child: Container(
+              margin: EdgeInsets.only(right: context.width / 30),
+              width: context.width / 3,
+              height: context.height / 1.2,
+              decoration: BoxDecoration(
+                color: ColorManager.white,
+                // borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                    offset: const Offset(1, -1),
+                    color: ColorManager.brown.withOpacity(0.5),
+                    blurRadius: 5,
+                    spreadRadius: 2,
+                  ),
+                ],
+              ),
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    _buildJuiceList(context),
+                  ],
+                ),
+              ),
             ),
           ),
         ],
       ),
     );
+  }
+
+  Widget _buildJuiceForm(BuildContext context) {
+    return SizedBox(
+      width: context.width / 3,
+      child: Column(
+        children: [
+          MyTextFormField(
+            controller: _nameController,
+            hintText: 'Juice Name',
+          ),
+          MyTextFormField(
+            height: 5,
+            controller: _priceController,
+            keyboardType: TextInputType.number,
+            hintText: 'Price',
+          ),
+          SizedBox(height: 20.h),
+          MyElevatedButton(
+            title: 'Add Juice',
+            onPressed: () {
+              if (_nameController.text.isNotEmpty &&
+                  _priceController.text.isNotEmpty &&
+                  _pickedImage != null) {
+                final name = _nameController.text;
+                final price = int.tryParse(_priceController.text) ?? 0;
+                final image = _pickedImage!.path;
+                context
+                    .read<FruitsRepositoryCubit>()
+                    .addUserJuice(name, price, image);
+                _clearForm();
+              }
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildJuiceList(BuildContext context) {
+    return BlocBuilder<FruitsRepositoryCubit, List<JuiceModel>>(
+      builder: (context, juices) {
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: juices.map((juice) => _buildJuiceItem(juice)).toList(),
+        );
+      },
+    );
+  }
+
+  Widget _buildJuiceItem(JuiceModel juice) {
+    return ListTile(
+      leading: CircleAvatar(
+        radius: 40,
+        backgroundImage: FileImage(File(juice.image!)),
+      ),
+      title: Text(
+        juice.name,
+        style: context.textTheme.bodyLarge,
+      ),
+      subtitle: Text(
+        '\$${juice.price}',
+        style: context.textTheme.displayMedium,
+      ),
+      trailing: IconButton(
+        icon: Icon(
+          Icons.delete,
+          size: 7.sp,
+          color: ColorManager.brown,
+        ),
+        onPressed: () => _showConfirmationDialog(juice),
+      ),
+    );
+  }
+
+  void _showConfirmationDialog(JuiceModel juice) {
+    showDialog(
+      context: context,
+      builder: (BuildContext dialogContext) {
+        return AlertDialog(
+          backgroundColor: ColorManager.white,
+          title: Text(
+            'Remove ${juice.name}?',
+            style: context.textTheme.bodyLarge,
+          ),
+          content: Text(
+            'Are you sure you want to remove this juice?',
+            style: context.textTheme.displayMedium,
+          ),
+          actions: <Widget>[
+            TextButton(
+              style: ButtonStyle(
+                overlayColor: WidgetStatePropertyAll(
+                    ColorManager.correct.withOpacity(0.3)),
+              ),
+              onPressed: () {
+                Navigator.of(dialogContext).pop();
+              },
+              child: Text(
+                'Cancel',
+                style: context.textTheme.displayMedium
+                    ?.copyWith(color: ColorManager.correct),
+              ),
+            ),
+            TextButton(
+              style: ButtonStyle(
+                overlayColor:
+                    WidgetStatePropertyAll(ColorManager.error.withOpacity(0.3)),
+              ),
+              onPressed: () {
+                _removeJuice(juice);
+                Navigator.of(dialogContext).pop();
+              },
+              child: Text(
+                'Remove',
+                style: context.textTheme.displayMedium
+                    ?.copyWith(color: ColorManager.error),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _removeJuice(JuiceModel juice) {
+    context.read<FruitsRepositoryCubit>().removeUserJuice(juice.name);
+  }
+
+  void _clearForm() {
+    _nameController.clear();
+    _priceController.clear();
+    setState(() {
+      _pickedImage = null;
+    });
   }
 }
