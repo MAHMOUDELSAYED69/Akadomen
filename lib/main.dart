767a8e82583769a9b8cd46314f4ad1208178f3f4
MahmoudElSayed96 <gcfjxvkj@gmail.com>
2024-07-16 01:02:08 +0300

    add myapp class in a separate file.

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,10 @@
-import 'dart:async';
 import 'dart:io';
-
-import 'package:akadomen/router/app_router.dart';
-import 'package:akadomen/utils/constants/routes.dart';
+import 'package:akadomen/app.dart';
 import 'package:akadomen/utils/helpers/shared_pref.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'package:window_manager/window_manager.dart';
-
 import 'database/sql.dart';
-import 'utils/themes/app_theme.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -37,32 +31,4 @@ Future<void> main() async {
   }
   await SqlDb().initializeDatabase();
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return ScreenUtilInit(
-        minTextAdapt: true,
-        splitScreenMode: true,
-        builder: (_, child) => MaterialApp(
-              builder: (context, widget) {
-                final mediaQueryData = MediaQuery.of(context);
-                final scaledMediaQueryData = mediaQueryData.copyWith(
-                  textScaler: TextScaler.noScaling,
-                );
-                return MediaQuery(
-                  data: scaledMediaQueryData,
-                  child: widget!,
-                );
-              },
-              theme: AppTheme.lightTheme,
-              title: 'akadomen',
-              debugShowCheckedModeBanner: false,
-              initialRoute: RouteManager.initialRoute,
-              onGenerateRoute: AppRouter.onGenerateRoute,
-            ));
-  }
 }
