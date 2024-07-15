fda76700645f335d5a1b020263c4d282a3b07e6c
MahmoudElSayed96 <gcfjxvkj@gmail.com>
2024-07-15 22:28:04 +0300

    create deleteAndRecreateDatabase method, create invoices table.

--- lib/database/sql.dart
+++ lib/database/sql.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/foundation.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
@@ -26,47 +27,91 @@ class SqlDb {
     String path = join(databasePath, 'akadomen.db');
     Database mydb = await openDatabase(
       path,
-      version: 3,
+      version: 2,
       onCreate: _onCreate,
-      onUpgrade: _onUpgrade,
     );
     return mydb;
   }
 
+  Future<void> deleteAndRecreateDatabase() async {
+    String databasePath = await getDatabasesPath();
+    String path = join(databasePath, 'akadomen.db');
+    await deleteDatabase(path);
+  }
+
   Future<void> _onCreate(Database db, int version) async {
+    //? Create 'users' table
     await db.execute('''
-    CREATE TABLE "users" (
-      "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-      "username" TEXT NOT NULL,
-      "password" TEXT NOT NULL,
-      "login_status" BOOLEAN NOT NULL,
-      "invoice_number" INTEGER DEFAULT 0
-    )
+      CREATE TABLE users (
+        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+        username TEXT NOT NULL,
+        password TEXT NOT NULL,
+        login_status BOOLEAN NOT NULL,
+        invoice_number INTEGER DEFAULT 0
+      )
     ''');
 
+    //? Create 'juices' table
     await db.execute('''
-    CREATE TABLE "juices" (
-      "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-      "name" TEXT NOT NULL,
-      "price" INTEGER NOT NULL,
-      "image" TEXT NOT NULL,
-      "username" TEXT NOT NULL,
-      FOREIGN KEY (username) REFERENCES users(username)
-    )
+      CREATE TABLE juices (
+        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        price INTEGER NOT NULL,
+        image TEXT NOT NULL,
+        username TEXT NOT NULL,
+        FOREIGN KEY (username) REFERENCES users(username)
+      )
     ''');
+
+    //? Create 'invoices' table
+    await db.execute('''
+  CREATE TABLE IF NOT EXISTS invoices (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    invoice_number INTEGER NOT NULL,
+    customer_name TEXT NOT NULL,
+    date TEXT NOT NULL,
+    time TEXT NOT NULL,
+    total_amount REAL NOT NULL,
+    items TEXT,  -- Add items column here
+    username TEXT NOT NULL,
+    FOREIGN KEY (username) REFERENCES users(username)
+  )
+''');
+
     debugPrint("DB Created!");
   }
 
-  Future<void> _onUpgrade(Database db, int oldVersion, int newVersion) async {
-    if (oldVersion < 3) {
-      await db.execute(
-          'ALTER TABLE users ADD COLUMN invoice_number INTEGER DEFAULT 0');
-    }
+  // Future<void> _onUpgrade(Database db, int oldVersion, int newVersion) async {
+  //   if (oldVersion < 3) {
+  //     await db.execute(
+  //         'ALTER TABLE users ADD COLUMN invoice_number INTEGER DEFAULT 0');
+  //     debugPrint("DB Upgraded to version 3!");
+  //   }
+  //   if (oldVersion < 4) {
+  //     await db.execute('''
+  //       CREATE TABLE IF NOT EXISTS new_table (
+  //         id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  //         name TEXT NOT NULL
+  //       )
+  //     ''');
+  //     debugPrint("DB Upgraded to version 4!");
+  //   }
+  // Add upgrade logic for other versions
+  // Example:
+  // if (oldVersion < 5) {
+  //   await db.execute('ALTER TABLE existing_table ADD COLUMN new_column TEXT');
+  //   debugPrint("DB Upgraded to version 5!");
+  // }
+  // }
+
+  Future<void> saveInvoice(Map<String, dynamic> invoiceData) async {
+    Database? mydb = await db;
+    await mydb?.insert('invoices', invoiceData);
   }
 
-  Future<List<Map<String, Object?>>?> readData({required String data}) async {
+  Future<List<Map<String, dynamic>>?> readData({required String data}) async {
     Database? mydb = await db;
-    List<Map<String, Object?>>? response = await mydb?.rawQuery(data);
+    List<Map<String, dynamic>>? response = await mydb?.rawQuery(data);
     return response;
   }
 
@@ -99,10 +144,10 @@ class SqlDb {
     return response;
   }
 
-  Future<Map<String, Object?>?> getUser(
+  Future<Map<String, dynamic>?> getUser(
       String username, String password) async {
     Database? mydb = await db;
-    List<Map<String, Object?>>? response = await mydb?.query('users',
+    List<Map<String, dynamic>>? response = await mydb?.query('users',
         where: 'username = ? AND password = ?',
         whereArgs: [username, password]);
     return response?.isNotEmpty == true ? response?.first : null;
@@ -110,7 +155,7 @@ class SqlDb {
 
   Future<int?> getNextInvoiceNumber(String username) async {
     Database? mydb = await db;
-    List<Map<String, Object?>>? result = await mydb?.query('users',
+    List<Map<String, dynamic>>? result = await mydb?.query('users',
         columns: ['invoice_number'],
         where: 'username = ?',
         whereArgs: [username]);
