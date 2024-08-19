6902b11b01bb1cb4d048ff40b1db4e37c36a1ecd
MahmoudElSayed96 <gcfjxvkj@gmail.com>
2024-08-19 10:58:30 +0300

    refactor FruitsRepository

--- lib/repositories/fruits.dart
+++ lib/repositories/fruits.dart
@@ -1,14 +1,14 @@
-import '../database/sql.dart';
+import '../database/hive.dart';
 import '../models/archive.dart';
 import '../models/juice.dart';
+import 'package:hive/hive.dart';
+
 import '../utils/constants/images.dart';
 
 class FruitsRepository {
   FruitsRepository._privateConstructor();
   static final FruitsRepository instance =
       FruitsRepository._privateConstructor();
-
-  final SqlDb _sqlDb = SqlDb();
 
   static List<JuiceModel> juiceList = [
     JuiceModel(name: 'Apple', price: 70, image: ImageManager.apple),
@@ -27,53 +27,68 @@ class FruitsRepository {
   ];
 
   Future<List<JuiceModel>> getUserJuices(String username) async {
-    final data = await _sqlDb.readData(
-        data: 'SELECT * FROM juices WHERE username = "$username"');
-    List<JuiceModel> userJuices = data
-            ?.map((item) => JuiceModel(
-                  name: item['name'] as String,
-                  price: item['price'] as int,
-                  image: item['image'] as String,
-                ))
-            .toList() ??
-        [];
+    final juicesBox = Hive.box<Juice>('juices');
+    List<Juice> userJuices =
+        juicesBox.values.where((juice) => juice.username == username).toList();
 
-    List<JuiceModel> mergedList = [...juiceList];
-    mergedList.addAll(userJuices);
+    // Map Hive Juice objects to JuiceModel
+    List<JuiceModel> userJuiceModels = userJuices
+        .map((juice) => JuiceModel(
+            name: juice.name, price: juice.price, image: juice.image))
+        .toList();
 
+    // Merging with predefined juice list
+    List<JuiceModel> mergedList = [...juiceList, ...userJuiceModels];
     return mergedList;
   }
 
   Future<void> addUserJuice(String username, JuiceModel juice) async {
-    final data = '''
-      INSERT INTO juices (name, price, image, username)
-      VALUES ('${juice.name}', ${juice.price}, '${juice.image}', "$username")
-    ''';
-    await _sqlDb.insertData(data: data);
+    final juicesBox = Hive.box<Juice>('juices');
+    Juice newJuice = Juice()
+      ..name = juice.name
+      ..price = juice.price
+      ..image = juice.image!
+      ..username = username;
+    await juicesBox.add(newJuice);
   }
 
   Future<void> removeUserJuice(String username, String juiceName) async {
-    final data = '''
-      DELETE FROM juices WHERE username = "$username" AND name = '$juiceName'
-    ''';
-    await _sqlDb.deleteData(data: data);
+    final juicesBox = Hive.box<Juice>('juices');
+    final juice = juicesBox.values.firstWhere(
+      (juice) => juice.username == username && juice.name == juiceName,
+    );
+
+    await juice.delete();
   }
 
   Future<void> updateJuicePrice(
       String username, String juiceName, int newPrice) async {
-    final data = '''
-      UPDATE juices SET price = $newPrice WHERE username = "$username" AND name = '$juiceName'
-    ''';
-    await _sqlDb.updateData(data: data);
+    final juicesBox = Hive.box<Juice>('juices');
+    final juice = juicesBox.values.firstWhere(
+      (juice) => juice.username == username && juice.name == juiceName,
+    );
+
+    juice.price = newPrice;
+    await juice.save();
   }
 
   Future<List<ArchiveModel>?> getInvoices(String username) async {
-    final data = '''
-    SELECT * FROM invoices WHERE username = "$username"
-    ''';
-    final List<Map<String, Object?>>? result =
-        await _sqlDb.readData(data: data);
-    return result?.map((e) => ArchiveModel.fromMap(e)).toList();
+    final invoicesBox = Hive.box<Invoice>('invoices');
+    List<Invoice> userInvoices = invoicesBox.values
+        .where((invoice) => invoice.username == username)
+        .toList();
+
+    return userInvoices
+        .map((invoice) => ArchiveModel.fromMap({
+              'invoice_number': invoice.invoiceNumber,
+              'customer_name': invoice.customerName,
+              'date': invoice.date,
+              'time': invoice.time,
+              'total_amount': invoice.totalAmount,
+              'items': invoice.items,
+              'username': invoice.username,
+            }))
+        .toList();
   }
 
   Future<void> saveInvoice({
@@ -85,10 +100,15 @@ class FruitsRepository {
     required String items,
     required String userName,
   }) async {
-    final data = '''
-    INSERT INTO invoices (invoice_number, customer_name, date, time, total_amount, items, username)
-    VALUES ("$invoiceNumber", "$customerName", "$formattedDate", "$formattedTime", $totalAmount, "$items", "$userName")
-    ''';
-    await _sqlDb.insertData(data: data);
+    final invoicesBox = Hive.box<Invoice>('invoices');
+    Invoice newInvoice = Invoice()
+      ..invoiceNumber = invoiceNumber
+      ..customerName = customerName
+      ..date = formattedDate
+      ..time = formattedTime
+      ..totalAmount = totalAmount
+      ..items = items
+      ..username = userName;
+    await invoicesBox.add(newInvoice);
   }
 }
